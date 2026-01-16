61b43a8413054fabd582dc5f58d8f5666fdb0354
carsolano71 <45107757+carsolano71@users.noreply.github.com>
2026-01-16 10:56:21 -0500

    Refactor example_silver_dag for new SQL and settings
    Updated SQL path and DAG configuration for customers.

--- dags/silver/example_silver_dag.py
+++ dags/silver/example_silver_dag.py
@@ -3,26 +3,26 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobO
 from datetime import datetime
 from pathlib import Path
 
-# Ruta al SQL de Silver
-SQL_PATH = Path(__file__).parents[2] / "sql" / "silver" / "customers_silver.sql"
+# Ruta al SQL
+SQL_PATH = Path(__file__).parents[2] / "sql" / "silver" / "customers.sql"
 
 with DAG(
-    dag_id="customers_silver_dag",
+    dag_id="example_silver_dag",
     start_date=datetime(2024, 1, 1),
-    schedule_interval=None,  # ejecución manual (ideal para pruebas)
+    schedule_interval=None,
     catchup=False,
-    tags=["silver", "customers"],
+    tags=["bronze-to-silver", "customers", "silver"],
 ) as dag:
 
     run_customers_silver = BigQueryInsertJobOperator(
-        task_id="run_customers_silver_sql",
+        task_id="run_customers_silver",
+        project_id="my-project-cash-new",
+        location="US",
         configuration={
             "query": {
                 "query": SQL_PATH.read_text(),
                 "useLegacySql": False,
+                "writeDisposition": "WRITE_TRUNCATE",
             }
         },
-        location="US",  # usa la región de tu BigQuery
     )
-
-    run_customers_silver
